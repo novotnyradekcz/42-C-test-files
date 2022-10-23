25de3b549ba7fcb9f5f0bd2ea39a87e341832d17
Radek Novotny <rnovotny@c3r6s4.42prague.com>
2022-10-23 20:10:54 +0200

    Updated C_04

--- C_04/main.c
+++ C_04/main.c
@@ -17,6 +17,7 @@
 #include "ex02/ft_putnbr.c"
 #include "ex03/ft_atoi.c"
 #include "ex04/ft_putnbr_base.c"
+#include "ex05/ft_atoi_base.c"
 
 int	main()
 {
@@ -49,15 +50,29 @@ int	main()
 
 	// ex03
 	printf("\n\nex03\n");
-	printf("%d\n", ft_atoi("  ++---2147483648"));
-	printf("%d\n", ft_atoi("46786"));
+	printf("%d\n", ft_atoi("  \t++---2147483648"));
+	printf("%d\n", ft_atoi(" -  46786"));
 	printf("%d\n", ft_atoi("  ++--1hgh234"));
 
 	// ex04
 	printf("\nex04\n");
-	ft_putnbr_base(26, "0123");
+	write(1, "\nNumber 2147483647 in base 0123456789: ", 40);
+	ft_putnbr_base(-2147483648, "0123456789");
+	write(1, "\nNumber -2147483648 in base zabcdefghi: ", 41);
+	ft_putnbr_base(-2147483648, "zabcdefghi");
+	write(1, "\nNumber 0 in base yrfji: ", 26);
+	ft_putnbr_base(0, "yrfji");
 
 	// ex05
 	printf("\n\nex05\n");
+	char num1[] = "  \t++---badgdhcfdh";
+	char base1[] = "uabcdefghi";
+	char num2[] = " -  46786";
+	char base2[] = "0123456789";
+	char num3[] = " ++--1hgh234";
+	char base3[] = "0123456789";
+	printf("Number %s, base %s, converted to decimal: %d\n", num1, base1, ft_atoi_base(num1, base1));
+	printf("Number %s, base %s, converted to decimal: %d\n", num2, base2, ft_atoi_base(num2, base2));
+	printf("Number %s, base %s, converted to decimal: %d\n", num3, base3, ft_atoi_base(num3, base3));
 	
 }
